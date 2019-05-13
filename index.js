9d5c20433907f17ad7b45c6b406d78e60756839c
rodrigoh00per <rodrigoh00per1@gmail.com>
2019-05-13 01:22:43 -0500

    Se hicieron los cambios finales para dejarlo en produccion

--- index.js
+++ index.js
@@ -10,14 +10,24 @@ const url_mongo = process.env.MONGO_URL;
 /* ------------------------------------------------------------------------------------------- */
 /* ------------------------------------------------------------------------------------------- */
 
+const dotenv = require("dotenv");
+
+const result = dotenv.config();
+
+try {
+  if (result.error) throw result.error;
+} catch (e) {
+  console.log(e);
+}
+
 //aqui se levanta el servidor con express
-app.listen(puerto, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log("SERVIDOR LEVANTADO EN EL PUERTO:" + process.env.PORT);
 });
 
 //aqui se levanta el servidor de mongo
 mongoose
-  .connect("mongodb://localhost:27017/EntregandoDB")
+  .connect(process.env.MONGO_URL)
   .then(() => {
     console.log("Base de datos levantada en el puerto 27017");
   })
